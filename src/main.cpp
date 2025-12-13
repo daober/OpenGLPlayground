3a3ca3a8b3793b4212b50203eccdbad97d654441
Daniel Obermaier <dan.obermaier@gmail.com>
2025-12-13 13:48:20 +0100

    cool stuff

--- src/main.cpp
+++ src/main.cpp
@@ -101,13 +101,13 @@ int main()
     // ================== LANE QUAD ==================
     float laneQuad[] = {
         // positions (X, Y, Z)
-        -5.0f, 0.0f,  0.0f,   // left  near
-        5.0f, 0.0f,  0.0f,   // right near
-        5.0f, 0.0f, -10.0f,  // right far
+        -5.0f, -0.49f, -0.5f,   // left  near  (offset forward)
+        5.0f, -0.49f, -0.5f,   // right near
+        5.0f, -0.49f, -10.5f,  // right far
 
-        -5.0f, 0.0f,  0.0f,   // left  near
-        5.0f, 0.0f, -10.0f,  // right far
-        -5.0f, 0.0f, -10.0f   // left  far
+        -5.0f, -0.49f, -0.5f,
+        5.0f, -0.49f, -10.5f,
+        -5.0f, -0.49f, -10.5f
     };
 
     unsigned int laneVAO, laneVBO;
